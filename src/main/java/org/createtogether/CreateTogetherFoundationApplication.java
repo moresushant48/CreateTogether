375850b8e1d6db18742639fde42145ad30391d8b
Sushant More <moresushant48@gmail.com>
2020-05-26 12:06:13 +0530

    WebApp : Project Startup.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

---
+++ src/main/java/org/createtogether/CreateTogetherFoundationApplication.java
@@ -0,0 +1,13 @@
+package org.createtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateTogetherFoundationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateTogetherFoundationApplication.class, args);
+	}
+
+}
