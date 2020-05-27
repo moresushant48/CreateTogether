c2ee8e9c05c703116b878cdc060754d27c1c009c
Sushant More <moresushant48@gmail.com>
2020-05-27 12:04:48 +0530

    Enitiy/Volunteer : Set onCreate, onUpdate DateTime automatically.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/main/java/org/createtogether/Models/Volunteer.java
+++ src/main/java/org/createtogether/Models/Volunteer.java
@@ -1,10 +1,15 @@
 package org.createtogether.Models;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Entity
 @Table(name = "Volunteers")
@@ -21,7 +26,13 @@ public class Volunteer {
 	private String vEmailId;
 	
 	private String vAddress;
-
+	
+	@CreationTimestamp
+    private LocalDateTime createDateTime;
+ 
+    @UpdateTimestamp
+    private LocalDateTime updateDateTime;
+    
 	public int getvId() {
 		return vId;
 	}
@@ -61,5 +72,20 @@ public class Volunteer {
 	public void setvAddress(String vAddress) {
 		this.vAddress = vAddress;
 	}
-	
+
+	public LocalDateTime getCreateDateTime() {
+		return createDateTime;
+	}
+
+	public void setCreateDateTime(LocalDateTime createDateTime) {
+		this.createDateTime = createDateTime;
+	}
+
+	public LocalDateTime getUpdateDateTime() {
+		return updateDateTime;
+	}
+
+	public void setUpdateDateTime(LocalDateTime updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
 }
