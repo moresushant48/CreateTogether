74154fff4494c6b8d97e8e387eee8e5099c70bf7
Sushant More <moresushant48@gmail.com>
2020-05-27 17:18:53 +0530

    CORS : Allow CrossOrigin request [2]
    Spring SecurityConfiguration was still blocking it on some requests, Fixed now.
    
    prev commit : https://github.com/moresushant48/CreateTogether/commit/1a46d0cba015a6c611ee173997f2f1980e0d4b6a
    
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/main/java/org/createtogether/SecurityConfiguration.java
+++ src/main/java/org/createtogether/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package org.createtogether;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -19,12 +20,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
-		http.authorizeRequests()
+		http
+		.authorizeRequests()
 		.antMatchers("/").permitAll()
-		.antMatchers("/api/**").permitAll();
+		.antMatchers("/api/**").permitAll()
+		
+		// CORS setup.
+		.and()
+		.cors().and();
+		
 		super.configure(http);
 	}
 	
-	
-	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// TODO Auto-generated method stub
+		web.ignoring().antMatchers("/api/**");
+	}	
 }
