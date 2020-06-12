25729d8fc45aac99bf0bf860758ef129bcd25c00
Sushant More <moresushant48@gmail.com>
2020-06-12 14:18:04 +0530

    Admin : Can fetch Moderator(s) seperately.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/main/java/org/createtogether/Repository/UserRepository.java
+++ src/main/java/org/createtogether/Repository/UserRepository.java
@@ -1,7 +1,9 @@
 package org.createtogether.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.createtogether.Models.ERole;
 import org.createtogether.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,5 +12,9 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	Boolean existsByEmail(String email);
 	
 	Optional<User> findByEmail(String email);
+
+	List<User> findAllByRolesName(ERole roleModerator);
+
+	User findByIdAndRolesName(int userId, ERole roleModerator);
 
 }
--- src/main/java/org/createtogether/RestControllers/AdminController.java
+++ src/main/java/org/createtogether/RestControllers/AdminController.java
@@ -1,5 +1,7 @@
 package org.createtogether.RestControllers;
 
+import java.util.List;
+
 import org.createtogether.Models.ERole;
 import org.createtogether.Models.User;
 import org.createtogether.Repository.RoleRepository;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,10 +35,28 @@ public class AdminController {
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
-	@PostMapping("/addMod")
+	@PostMapping("/post/addMod")
 	public ResponseEntity<?> registerUser(@RequestBody User user) {
 		
 		return authService.registerUser(user, ERole.ROLE_MODERATOR);
+	}
+	
+	@GetMapping("/get/mods")
+	public List<User> getModerators() {
+		
+		List<User> mods = userRepository.findAllByRolesName(ERole.ROLE_MODERATOR);
+		for(int i=0; i < mods.size(); i++) { mods.get(i).setPassword("null"); }
+		
+		return mods;
+	}
+	
+	@GetMapping("/get/mods/{userId}")
+	public User getModerators(@PathVariable("userId") int userId) {
+		
+		User mod = userRepository.findByIdAndRolesName(userId, ERole.ROLE_MODERATOR);
+		mod.setPassword("null");
+		
+		return mod;
 	}
 	
 }
