c99206beb2bcfca367407f15dc5a87be666f1616
Sushant More <moresushant48@gmail.com>
2020-08-11 14:14:37 +0530

    Post : Create Seperate PostService and move code from controller to service.
    Signed-off-by: Sushant More <moresushant48@gmail.com>

--- src/main/java/org/createtogether/RestControllers/PostController.java
+++ src/main/java/org/createtogether/RestControllers/PostController.java
@@ -1,21 +1,12 @@
 package org.createtogether.RestControllers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
-import org.createtogether.Models.Category;
-import org.createtogether.Models.ECategory;
 import org.createtogether.Models.Post;
-import org.createtogether.Models.User;
-import org.createtogether.Repository.CategoryRepository;
-import org.createtogether.Repository.PostRepository;
-import org.createtogether.Repository.RoleRepository;
-import org.createtogether.Repository.UserRepository;
-import org.createtogether.Security.jwt.MessageResponse;
+
+import org.createtogether.Services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,116 +18,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
-
-	@Autowired
-	UserRepository userRepository;
-
-	@Autowired
-	RoleRepository roleRepository;
 	
 	@Autowired
-	CategoryRepository categoryRepository;
-	
-	@Autowired
-	PostRepository postRepository;
+	PostService postService;
 	
 	@PostMapping("/add")
 	public ResponseEntity<?> addPosts(@RequestBody Post post) {
 		
-		post.setCategory(categoryRepository.findByctgTitle(ECategory.UNSPECIFIED).get());
-		
-		User user = userRepository.getOne(Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getName()));
-		if (user!=null) post.setPostUser(user);
-		else return ResponseEntity.badRequest().body(new MessageResponse("Something went wrong..! Please login again."));
-		
-		if(postRepository.save(post) != null)
-			return ResponseEntity.ok(new MessageResponse("Blog has been sent. Wait for Approval."));
-		
-		return ResponseEntity.badRequest().body(new MessageResponse("Couldn't process your post."));
+		return postService.addPosts(post);
 	}
 	
 	@PatchMapping("/patch/{postId}/{category}")
 	public ResponseEntity<?> updatePostCategory(@PathVariable("postId") int postId, @PathVariable("category") String category) {
-				
-		Post post = postRepository.getOne(postId);
-		
-		if(post!=null) {
-			
-			post.setPostPublished(false);
-			
-			if(category.equalsIgnoreCase(ECategory.EVENTS.toString()))
-				post.setCategory(categoryRepository.findByctgTitle(ECategory.EVENTS).get());
-			else if (category.equalsIgnoreCase(ECategory.PROGRAMS.toString())) 
-					post.setCategory(categoryRepository.findByctgTitle(ECategory.PROGRAMS).get());
-				else {
-					post.setCategory(categoryRepository.findByctgTitle(ECategory.UNSPECIFIED).get());
-					post.setPostPublished(false);
-				}
-			
-			if(postRepository.save(post)!= null) {
-				return ResponseEntity.ok(new MessageResponse("Post category updated."));
-			}
-			
-		}else return ResponseEntity.badRequest().body(new MessageResponse("Couldn't find post."));
-		
-		return ResponseEntity.badRequest().body(new MessageResponse("Couldn't update category."));
+
+		return postService.updatePostCategory(postId, category);
 	}
 	
 	@GetMapping("/get/unspecified")
 	public List<Integer> getUnspecifiedPosts() {
 		
-		Category unspecified = categoryRepository.findByctgTitle(ECategory.UNSPECIFIED).get();
-		
-		try {
-			User user = userRepository.getOne(Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getName()));
-			if(user!=null) {
-				if(user.getRoles().contains(roleRepository.getOne(1))) 
-					return postRepository.getIdsByCategory(unspecified).get();
-			}
-		}catch (NoSuchElementException e) {
-			System.out.println("No Pending Articles Found.");
-		}
-		
-		return new ArrayList<Integer>();
+		return postService.getUnspecifiedPosts();
 	}
 	
 	@GetMapping("/get/programs")
 	public List<Post> getPosts() {
 		
-		Category programs = categoryRepository.findByctgTitle(ECategory.PROGRAMS).get();
-		
-		try {
-			User user = userRepository.getOne(Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getName()));
-			if(user!=null) {
-				if(user.getRoles().contains(roleRepository.getOne(1))) {
-					return postRepository.findAllByCategory(programs).get();
-				} else
-					return postRepository.findAllBypostUserIdAndCategory(user.getId(), programs).get();
-			}
-		}catch (NoSuchElementException e) {
-			System.out.println("No Programs Found.");
-		}
-		
-		return new ArrayList<Post>();
+		return postService.getPosts();
 	}
 	
 	@GetMapping("/get/events")
 	public List<Post> getEvents() {
 		
-		Category events = categoryRepository.findByctgTitle(ECategory.EVENTS).get();
-		
-		try {
-			User user = userRepository.getOne(Integer.parseInt(SecurityContextHolder.getContext().getAuthentication().getName()));
-			if(user!=null) {
-				if(user.getRoles().contains(roleRepository.getOne(1))) {
-					return postRepository.findAllByCategory(events).get();
-				} else
-					return postRepository.findAllBypostUserIdAndCategory(user.getId(), events).get();
-			}
-		}catch (NoSuchElementException e) {
-			System.out.println("No Events Found.");
-		}
-		return new ArrayList<Post>();
+		return postService.getEvents();
 	}
 	
 }
